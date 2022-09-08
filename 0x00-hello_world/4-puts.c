c268ea29b5cf8b63426e319765058c7868b08c0a
MjKamma <Mercyigwe1995@gmail.com>
2022-09-08 14:56:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 
 /**
  * main - print the string in the puts function 
- *
  * discription: using the main function 
  * this program prints "Programming is like building a multilingual puzzle
  * return: 0
