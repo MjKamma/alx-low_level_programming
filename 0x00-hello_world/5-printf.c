ad7d23837c45957527c72d3dea68df910cfc6bdc
MjKamma <Mercyigwe1995@gmail.com>
2022-09-08 16:21:25 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- * Description: Print output with printf function. 
+ * Description: Print output with printf function.
  * Return: Always 0 (Success)
 */
 int main(void)
-{ 
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
