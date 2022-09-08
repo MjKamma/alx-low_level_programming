bed768c428d884f8fb9b05bf938f6aa9ce66ec17
MjKamma <Mercyigwe1995@gmail.com>
2022-09-08 14:15:54 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stadio.h>
+
+/**
+ *  main - print the string in the puts function   
+ *
+ *  discription: using the main function 
+ *  this program prints "Programming is like building a multilingual puzzle
+ *  return: 0
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+		return(0);
+}
