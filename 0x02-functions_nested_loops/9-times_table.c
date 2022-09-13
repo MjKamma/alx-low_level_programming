29e7d61521f3b7c79eb049a06501389133d837fa
MjKamma <Mercyigwe1995@gmail.com>
2022-09-13 16:24:32 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,19 +1,17 @@
-#include "main.h"
+#include"main.h"
 
-/*
- * times_table - Prints the 9 times table, starting with 0.
- * Description: function that prints the 9 times table,
+/**
+ * times_table - A code that prints the 9 times table, starting with 0.
 */
 
 void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num <= 9; ++num)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
+		_putchar(48);
+		for (mult = 1; mult <= 9; ++mult)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -23,9 +21,9 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + '0');
+				_putchar((prod / 10) + 48);
 
-			_putchar((prod % 10) + '0');
+			_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
