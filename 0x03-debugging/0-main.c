30441ca1b9ffa3393a6d6439a115f7ed70c7b919
MjKamma <Mercyigwe1995@gmail.com>
2022-09-15 11:09:53 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * main - test for a positive or nageative function
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_nagative(i);
+
+	return (0);
+}
